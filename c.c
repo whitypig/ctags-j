bf934c1bc78b833033fcc33d16b0c0002cc8e5b9
whitypig <whitypig@gmail.com>
2013-03-11 20:07:47 +0900

    Tidied up code.

--- c.c
+++ c.c
@@ -860,9 +860,7 @@ static javaKind javaTagKind (const tagType type)
 		case TAG_METHOD:     result = JK_METHOD;        break;
 		case TAG_PACKAGE:    result = JK_PACKAGE;       break;
 
-		default:
-			Assert ("Bad Java tag type" == NULL);
-      break;
+		default: Assert ("Bad Java tag type" == NULL); break;
 	}
 	return result;
 }
